cadbd93219d7ac156b76dc7159eefc6dd8f7a606
puria <135986609+purigithub@users.noreply.github.com>
2023-07-30 16:24:33 +0330

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0xd6c955da90275d1ec1dda05201738d93bdfb5da5";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
